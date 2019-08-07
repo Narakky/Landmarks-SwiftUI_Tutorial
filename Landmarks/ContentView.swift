4847a48c7f9c946f3e66f5a1a031e0c123809861
Narakky <11222997+Narakky@users.noreply.github.com>
2019-08-07 15:59:17 +0900

    initial commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Naraki on 8/7/19.
+//  Copyright © 2019 i-enter. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
